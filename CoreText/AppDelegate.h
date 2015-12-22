f87834455695e7add6bae5a380fe8efd248016ba
HeJianghao <jianghaocool2006@163.com>
2015-12-22 17:43:20 +0800

    Initial Commit

---
+++ CoreText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreText
+//
+//  Created by 何江浩 on 15/12/22.
+//  Copyright © 2015年 何江浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreText
+//
+//  Created by 何江浩 on 15/12/22.
+//  Copyright © 2015年 何江浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
